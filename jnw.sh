c28e3f4acca88f53eab49b526ced5e29f21a0ed2
Christian Schuhegger <Christian.Schuhegger@gmx.de>
2018-05-02 09:02:49 +0200

    jnw.sh

--- jnw.sh
+++ jnw.sh
@@ -7,7 +7,7 @@ dir=`pwd`
 TAG_DATE=201804191153
 IMAGE=jupyter-docker-base-image:$TAG_DATE
 
-docker run -it --rm -v ${dir}:/home/jovyan/work --pid=host --net=host --user root -e NB_UID=$UID -e NB_GID=$(getent group docker | awk -F: '{printf "%d", $3}') -e HTTP_PROXY=${HTTP_PROXY} -e HTTPS_PROXY=${HTTPS_PROXY} -e http_proxy=${HTTP_PROXY} -e https_proxy=${HTTPS_PROXY} -w /home/jovyan/work $IMAGE start-notebook.sh --NotebookApp.token=''
+echo docker run -it --rm -v ${dir}:/home/jovyan/work --pid=host --net=host --user root -e NB_UID=$UID -e NB_GID=$(getent group docker | awk -F: '{printf "%d", $3}') -e HTTP_PROXY=${HTTP_PROXY} -e HTTPS_PROXY=${HTTPS_PROXY} -e http_proxy=${HTTP_PROXY} -e https_proxy=${HTTPS_PROXY} -w /home/jovyan/work $IMAGE start-notebook.sh --NotebookApp.token=''
 docker run -it --rm -v ${dir}:/home/jovyan/work --pid=host --net=host --user root -e NB_UID=$UID -e NB_GID=$(getent group docker | awk -F: '{printf "%d", $3}') -e HTTP_PROXY=${HTTP_PROXY} -e HTTPS_PROXY=${HTTPS_PROXY} -e http_proxy=${HTTP_PROXY} -e https_proxy=${HTTPS_PROXY} -w /home/jovyan/work $IMAGE start-notebook.sh --NotebookApp.token=''
 
 
