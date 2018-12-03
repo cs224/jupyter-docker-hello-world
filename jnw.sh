14b2af7cbc144061f72eb91f678b28d08a644ac4
Christian Schuhegger <Christian.Schuhegger@gmx.de>
2018-12-03 14:08:18 +0100

    Updated packages and set new TAG_DATE=201812030859

--- jnw.sh
+++ jnw.sh
@@ -4,7 +4,7 @@
 #dir=`dirname $dir`
 dir=`pwd`
 
-TAG_DATE=201804191153
+TAG_DATE=201812030859
 IMAGE=jupyter-docker-base-image:$TAG_DATE
 
 echo docker run -it --rm -v ${dir}:/home/jovyan/work --pid=host --net=host --user root -e NB_UID=$UID -e NB_GID=$(getent group docker | awk -F: '{printf "%d", $3}') -e HTTP_PROXY=${HTTP_PROXY} -e HTTPS_PROXY=${HTTPS_PROXY} -e http_proxy=${HTTP_PROXY} -e https_proxy=${HTTPS_PROXY} -w /home/jovyan/work $IMAGE start-notebook.sh --NotebookApp.token=''
--- rstudio.sh
+++ rstudio.sh
@@ -9,8 +9,7 @@
 
 dir=`pwd`
 
-TAG_DATE=201804191153
-#IMAGE=jupyter-docker-base-image:$TAG_DATE
+TAG_DATE=201812030859
 IMAGE=rstudio-docker-base-image:$TAG_DATE
 
 # docker run --rm -it --net=host --privileged --env="DISPLAY" -v $HOME/.Xauthority:/root/.Xauthority --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1" -v ${dir}:/home/jovyan/work --user root -e NB_UID=$UID -e NB_GID=$(getent group docker | awk -F: '{printf "%d", $3}') -e HTTP_PROXY=${HTTP_PROXY} -e HTTPS_PROXY=${HTTPS_PROXY} -e http_proxy=${HTTP_PROXY} -e https_proxy=${HTTPS_PROXY} -w /home/jovyan/work ubuntu:latest sh -c 'apt-get update && apt-get install -qqy x11-apps && xeyes'
