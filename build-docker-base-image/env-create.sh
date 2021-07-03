498a0f5c702b5a8768136961f786188428f3000e
cs224 <cs224@weisser-zwerg.dev>
2021-07-03 15:54:19 +0200

    build-docker-base-image/environment.yml

--- build-docker-base-image/env-create.sh
+++ build-docker-base-image/env-create.sh
@@ -32,7 +32,8 @@ pip install --upgrade pip # --user
 
 echo ">> Setting-up py38ds environemnt"
 # conda create -n py38ds python=3.8
-conda env create -f environment.yml
+mamba env create -f environment.yml
+#conda env create -f environment.yml
 
 echo ">> Activating py38ds environment"
 CONDA_BASE=$(conda info --base)
@@ -43,10 +44,14 @@ echo ">> Installing scs"
 # conda install -c conda-forge scs
 mamba install -c conda-forge scs
 
+echo ">> Installing libgit2 and pystan from conda-forge"
+mamba install -n py38ds -c conda-forge libgit2 pystan
+# conda install -y -n py38ds -c conda-forge libgit2 pystan r-biocmanager
+
 echo ">> Installing pip installable packages"
 pip install -r requirements.txt
 
-echo ">> Installing pymc3"
+# echo ">> Installing pymc3"
 # mamba repoquery depends aesara -c conda-forge -t
 # mamba install -y -n py38ds -c conda-forge aesara -vv
 # -v or -vv to add verbosity as long as your version of conda is up-to-date. Older versions, you'll have to use --debug
@@ -55,11 +60,9 @@ echo ">> Installing pymc3"
 # conda install -y -n py38ds -c conda-forge pymc3=3.11.2
 # pip install pymc3
 
-echo ">> Installing libgit2 and pystan from conda-forge"
-mamba install -y -n py38ds -c conda-forge libgit2 pystan r-biocmanager
-# conda install -y -n py38ds -c conda-forge libgit2 pystan r-biocmanager
 
 echo ">> Installing additional R packages"
+mamba install -n py38ds -c conda-forge r-biocmanager
 # echo "r <- getOption('repos'); r['CRAN'] <- 'https://cloud.r-project.org'; options(repos = r);" > ~/.Rprofile
 Rscript install-1.R
 
@@ -69,11 +72,18 @@ Rscript install-2.R
 Rscript install-2.R
 
 echo ">> Installing pgmpy"
-mamba install -y -n py38ds -c ankurankan pgmpy
+# mamba install -y -n py38ds -c ankurankan pgmpy
 # conda install -y -n py38ds -c ankurankan pgmpy
 
 echo ">> Installing libpgm"
-pip --no-cache-dir install git+https://github.com/CyberPoint/libpgm@master
+# pip --no-cache-dir install git+https://github.com/CyberPoint/libpgm@master
+# pip uninstall numpy
+# pip install numpy==1.19.2
+# pip install daal==2021.1.2
+# pip install dpcpp_cpp_rt==2021.1.2
+
+#conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+# mamba install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
 
 conda info --envs
 # conda list
